02a39a2bd582eeecba194f97ed369b73cf4f5426
Mark <git@happybeing.com>
2019-12-23 17:25:56 +0000

    Fix drag position when zoomed in/out, replace all tabs

--- src/NetworkGraphD3SVG.svelte
+++ src/NetworkGraphD3SVG.svelte
@@ -5,126 +5,103 @@
 or touch screen. Note: on small touch screens you may need to 
 increase hit radius to hit a node with a 'fat finger'!</p>
 <script>
-	import { onMount } from 'svelte';
+    import { onMount } from 'svelte';
 
-	import { scaleLinear, scaleOrdinal } from 'd3-scale';
+    import { scaleLinear, scaleOrdinal } from 'd3-scale';
     import { zoom, zoomIdentity } from 'd3-zoom';
-	import { schemeCategory10 } from 'd3-scale-chromatic';
-	import { select, selectAll } from 'd3-selection';
-	import { drag } from 'd3-drag';
-	import { forceSimulation, forceLink, forceManyBody, forceCenter } from 'd3-force';
+    import { schemeCategory10 } from 'd3-scale-chromatic';
+    import { select, selectAll } from 'd3-selection';
+    import { drag } from 'd3-drag';
+    import { forceSimulation, forceLink, forceManyBody, forceCenter } from 'd3-force';
 
-	import {event as currentEvent} from 'd3-selection'  // Needed to get drag working, see: https://github.com/d3/d3/issues/2733
-	let d3 = { zoom, zoomIdentity, scaleLinear, scaleOrdinal, schemeCategory10, select, selectAll, drag,  forceSimulation, forceLink, forceManyBody, forceCenter }
+    import {event as currentEvent} from 'd3-selection'  // Needed to get drag working, see: https://github.com/d3/d3/issues/2733
+    let d3 = { zoom, zoomIdentity, scaleLinear, scaleOrdinal, schemeCategory10, select, selectAll, drag,  forceSimulation, forceLink, forceManyBody, forceCenter }
 
-	export let graph;
+    export let graph;
 
-	let width = 500;
-	let height = 600;
+    let width = 500;
+    let height = 600;
     const nodeRadius = 5;
 
-	const padding = { top: 20, right: 40, bottom: 40, left: 25 };
+    const padding = { top: 20, right: 40, bottom: 40, left: 25 };
 
-	$: xScale = scaleLinear()
-		.domain([0, 20])
-		.range([padding.left, width - padding.right]);
+    $: links = graph.links.map(d => Object.create(d));
+    $: nodes = graph.nodes.map(d => Object.create(d));  
 
-	$: yScale = scaleLinear()
-		.domain([0, 12])
-		.range([height - padding.bottom, padding.top]);
+    const colourScale = d3.scaleOrdinal(d3.schemeCategory10);
 
-	$: d3yScale = scaleLinear()
-		.domain([0, height])
-		.range([height, 0]);
-
-	$: links = graph.links.map(d => Object.create(d));
-	$: nodes = graph.nodes.map(d => Object.create(d));  
-
-	const colourScale = d3.scaleOrdinal(d3.schemeCategory10);
-
-	let transform = d3.zoomIdentity;
     let simulation, svg;
-	onMount(() => {
-		svg = d3.select(".chart")
-			.append("svg")
-			.attr("width", width)
-			.attr("height", height)
-			// .attr("viewBox", [0, 0, width, height]);
+    onMount(() => {
+        simulation = d3.forceSimulation(nodes)
+            .force("link", d3.forceLink(links).id(d => d.id))
+            .force("charge", d3.forceManyBody())
+            .force("center", d3.forceCenter(width / 2, height / 2))
+            .on('tick', simulationUpdate);
 
-		simulation = d3.forceSimulation(nodes)
-			.force("link", d3.forceLink(links).id(d => d.id))
-			.force("charge", d3.forceManyBody())
-			.force("center", d3.forceCenter(width / 2, height / 2))
-			.on('tick', simulationUpdate);
+        svg = d3.select(".chart")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+  
+        const g = svg.append("g");    // Need container when combininig drag and zoom
+                                    // See example: https://observablehq.com/@d3/drag-zoom
 
-		const link = svg.append("g")
-			.attr("stroke", "#999")
-			.attr("stroke-opacity", 0.6)
-			.selectAll("line")
-			.data(links)
-			.join("line")
-			.attr("stroke-width", d => Math.sqrt(d.value));
+        const link = g.append("g")
+            .attr("stroke", "#999")
+            .attr("stroke-opacity", 0.6)
+            .selectAll("line")
+            .data(links)
+            .join("line")
+            .attr("stroke-width", d => Math.sqrt(d.value));
 
-		const node = svg.append("g")
-			.attr("stroke", "#fff")
-			.attr("stroke-width", 1.5)
-			.selectAll("circle")
-			.data(nodes)
-			.join("circle")
-			.attr("r", 5)
-			.attr("fill", d => colourScale(d.group))
-			.call(d3.drag()
-				.on("start", dragstarted)
-				.on("drag", dragged)
-				.on("end", dragended))
+        const node = g.append("g")
+            .attr("stroke", "#fff")
+            .attr("stroke-width", 1.5)
+            .selectAll("circle")
+            .data(nodes)
+            .join("circle")
+            .attr("r", 5)
+            .attr("fill", d => colourScale(d.group))
+            .call(d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended))
 
-		node.append("title")
-			.text(d => d.id);
+        node.append("title")
+            .text(d => d.id);
 
-		d3.select(".chart").call(d3.zoom()
-			.extent([[0, 0], [width, height]])
-			.scaleExtent([1 / 10, 8])
-			.on("zoom", zoomed));
+        svg.call(d3.zoom()
+                .extent([[0, 0], [width, height]])
+                .scaleExtent([1 / 10, 8])
+                .on("zoom", zoomed));
+            
+        function simulationUpdate () {
+            link
+                .attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y)
 
-		function zoomed() {
-			transform = currentEvent.transform;
-			simulationUpdate();
-		}
-
-		function simulationUpdate () {
-			link
-				.attr("x1", d => d.source.x)
-				.attr("y1", d => d.source.y)
-				.attr("x2", d => d.target.x)
-				.attr("y2", d => d.target.y)
-				.attr("transform", transform);
-
-			node
-				.attr("cx", d => d.x)
-				.attr("cy", d => d.y)
-				.attr("transform", transform);
-			}
-
-	});
-
-	function dragsubject() {
-        const node = simulation.find(transform.invertX(currentEvent.x), transform.invertY(currentEvent.y), nodeRadius);
-        if (node) {
-            node.x = transform.applyX(node.x);
-            node.y = transform.applyY(node.y);
+            node
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y)
+            }
+    
+        function zoomed() {
+            g.attr("transform", currentEvent.transform)
+            simulationUpdate();
         }
-        return node;
-	}
+    });
 
     function dragstarted() {
         if (!currentEvent.active) simulation.alphaTarget(0.3).restart();
-        currentEvent.subject.fx = transform.invertX(currentEvent.subject.x);
-        currentEvent.subject.fy = transform.invertY(currentEvent.subject.y);
+        currentEvent.subject.fx = currentEvent.x;
+        currentEvent.subject.fy = currentEvent.y;
     }
 
     function dragged() {
-        currentEvent.subject.fx = transform.invertX(currentEvent.x);
-        currentEvent.subject.fy = transform.invertY(currentEvent.y);
+        currentEvent.subject.fx = currentEvent.x;
+        currentEvent.subject.fy = currentEvent.y;
     }
 
     function dragended() {
@@ -133,9 +110,9 @@ increase hit radius to hit a node with a 'fat finger'!</p>
         currentEvent.subject.fy = null;
     }
 
-	function resize() {
-		({ width, height } = svg.getBoundingClientRect());
-	}
+    function resize() {
+        ({ width, height } = svg.getBoundingClientRect());
+    }
 
 
 </script>
@@ -143,15 +120,3 @@ increase hit radius to hit a node with a 'fat finger'!</p>
 <svelte:window on:resize='{resize}'/>
 
 <div class='chartdiv'></div>
-
-<style>
-	svg {
-		float: left;
-	}
-
-	circle {
-		stroke: #fff;
-    stroke-width: 1.5;
-	}
-
-</style>
